02758f7d15dc87ecd5f681e02d4454afa3def0aa
EC2 Default User <ec2-user@ip-172-31-29-237.ap-northeast-1.compute.internal>
2023-04-13 11:03:20 +0000

    create book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,21 @@
 class BooksController < ApplicationController
-  def new
+  before_action :is_matching_login_user, only: [:edit, :update]
+
+  def index
+    @user = current_user
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
+  end
+
+   private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,26 +1,25 @@
 class UserController < ApplicationController
   
   def show
-    @user = User.find(params[:id])
-    @post_images = @user.post_images.page(params[:page])
+    @user = current_user
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
   end
   
   def update
-    @user = User.find(params[:id])
+    @user = current_user
     @user.update(user_params)
     redirect_to user_path
   end
   
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
   
   def is_matching_login_user
-    user = User.find(params[:id])
+    user = current_user
     unless user.id == current_user.id
       redirect_to post_images_path
     end
