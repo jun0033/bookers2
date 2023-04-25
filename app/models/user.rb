d562afff7e55fe86040d9136ab14bb2701a6a31d
EC2 Default User <ec2-user@ip-172-31-29-237.ap-northeast-1.compute.internal>
2023-04-25 10:55:13 +0000

    フォロー追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,37 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-        has_many :books, dependent: :destroy
-        has_many :favorites, dependent: :destroy
-        has_many :book_comments,dependent: :destroy
-        has_one_attached :profile_image
+  has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments,dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_one_attached :profile_image
 
 
-        def get_profile_image(width,height)
-            unless profile_image.attached?
-              file_path = Rails.root.join('app/assets/images/default-image.jpg')
-              profile_image.attach(io: File.open(file_path), filename: 'default-image', content_type: 'image/jpeg')
-            end
-          profile_image.variant(resize_to_limit: [width,height]).processed
-        end
+  def get_profile_image(width,height)
+      unless profile_image.attached?
+        file_path = Rails.root.join('app/assets/images/default-image.jpg')
+        profile_image.attach(io: File.open(file_path), filename: 'default-image', content_type: 'image/jpeg')
+      end
+    profile_image.variant(resize_to_limit: [width,height]).processed
+  end
 
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
+  end
+  
   validates :name, presence: true, uniqueness: true, length: { minimum: 2,maximum: 20 }
   validates :introduction, length: { maximum: 50 }
 end
