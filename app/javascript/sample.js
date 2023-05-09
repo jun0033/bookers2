b030f6a0815a5488e4db5ae42e0e3b26feb41cde
jun0033 <129253550+jun0033@users.noreply.github.com>
2023-05-09 13:14:38 +0000

    count-up実装

---
+++ app/javascript/sample.js
@@ -0,0 +1,16 @@
+/*global $*/
+$(window).on('turbolinks:load', function() {
+    //要素が見えたときに実行する処理
+    $(".count-up").each(function(){
+      $(this).prop('Counter',0).animate({//0からカウントアップ
+            Counter: $(this).text()
+        }, {
+        // スピードやアニメーションの設定
+            duration: 4000,//数字が大きいほど変化のスピードが遅くなる。2000=2秒
+            easing: 'swing',//動きの種類。他にもlinearなど設定可能
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
